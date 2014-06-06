1cbacde871b100189eb42ea958d94e82dbe43441
Carlo Bertoldi <mcbain@tiscali.it>
2014-06-06 16:01:22 +0200

    Added a couple of useful git aliases.

--- git/aliases.sh
+++ git/aliases.sh
@@ -4,6 +4,8 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+git config --global alias.rb rebase
+git config --global alias.pi cherry-pick
 
 git config --global alias.unstage 'reset HEAD --'
 git config --global alias.last 'log -1 HEAD'
